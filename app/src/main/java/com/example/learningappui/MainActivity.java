8513f1cf463ef664e5157a9df1f4f3968ad6f818
kushvyas <codekv2@gmail.com>
2021-06-23 08:10:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/learningappui/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.learningappui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+  ImageView i1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        i1=findViewById(R.id.i1);
+
+        i1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),MainActivity2.class);
+                startActivity(intent);
+
+
+            }
+        });
+    }
+}
